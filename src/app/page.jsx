24e498f585fe791bd16bd9312a0e21ca33709a39
Rishi <rishimaheshwari040@gmail.com>
2025-03-05 09:47:39 +0530

    update laptop section

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import About from "@/components/core/home/About";
+import AppointmentSection from "@/components/core/home/AppointmentSection";
 import HereSection from "@/components/core/home/HeroSection";
 import Services from "@/components/core/home/Services";
 import WeightManagementBanner from "@/components/core/home/WeightManagementBanner";
@@ -12,6 +13,7 @@ const Home = () => {
       <About />
       <Services />
       <WeightManagementBanner />
+      <AppointmentSection />
     </div>
   );
 };
