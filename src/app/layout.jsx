2b00c254cc6b6706e38e55d7502f5515676f5447
Rishi <rishimaheshwari040@gmail.com>
2025-04-11 20:50:47 +0530

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,29 +1,33 @@
+"use client";
+import { useEffect } from "react";
 import Wrapper from "./wrapper";
 
 export default function RootLayout({ children }) {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+      new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+      j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+      'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-WZF4QCRL');`;
+    document.head.appendChild(script);
+  }, []);
+
   return (
     <html lang="en">
+      <head />
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-WZF4QCRL"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Wrapper>{children}</Wrapper>
       </body>
     </html>
   );
 }
-export const generateMetadata = () => {
-  return {
-    title: "VitalCare Health",
-    description: "VitalCare Health",
-    keywords: "VitalCare Health",
-    url: "http://vitalcarega.com",
-    image: "/android-chrome-192x192.png",
-
-    // Open Graph Tags
-    openGraph: {
-      type: "website",
-      url: "VitalCare Health",
-      title: "VitalCare Health",
-      description: "VitalCare Health",
-      image: "/android-chrome-192x192.png",
-    },
-  };
-};
