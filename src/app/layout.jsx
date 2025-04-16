1ab9f937bdb4288652720f6bd657f97e7df6e32c
Rishi <rishimaheshwari040@gmail.com>
2025-04-16 22:11:35 +0530

    add teg script

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,13 +4,27 @@ import Wrapper from "./wrapper";
 
 export default function RootLayout({ children }) {
   useEffect(() => {
-    const script = document.createElement("script");
-    script.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    const gtmScript = document.createElement("script");
+    gtmScript.innerHTML = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
       new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
       j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
       'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-WZF4QCRL');`;
-    document.head.appendChild(script);
+    document.head.appendChild(gtmScript);
+
+    const gaScript = document.createElement("script");
+    gaScript.src = "https://www.googletagmanager.com/gtag/js?id=G-23RW5NTL09";
+    gaScript.async = true;
+    document.head.appendChild(gaScript);
+
+    const gaInitScript = document.createElement("script");
+    gaInitScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-23RW5NTL09');
+    `;
+    document.head.appendChild(gaInitScript);
   }, []);
 
   return (
@@ -25,7 +39,6 @@ export default function RootLayout({ children }) {
             style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
-
         <Wrapper>{children}</Wrapper>
       </body>
     </html>
